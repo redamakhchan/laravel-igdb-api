f48bf1f0fd5d462237944c12a476793902d99e14
Reda MAKHCHAN <reda.makhchan@ibhorizons.com>
2020-12-17 11:14:57 +0100

    alpinejs for search dropdown

--- resources/views/livewire/search-dropdown.blade.php
+++ resources/views/livewire/search-dropdown.blade.php
@@ -1,8 +1,20 @@
-<div class="relative">
+<div class="relative" x-data="{ open: true }" @click.away="open = false">
     <input
         wire:model.debounce.300ms="search"
         type="text"
-        class="bg-gray-800 pl-8 text-sm rounded-full focus:outline-none focus:shadow-outline px-3 py-1 w-64" placeholder="Search...">
+        class="bg-gray-800 pl-8 text-sm rounded-full focus:outline-none focus:shadow-outline px-3 py-1 w-64"
+        placeholder="Search (Press '/' to focus)"
+        x-ref="search"
+        @keydown.window="
+            if(event.keyCode === 111) {
+                event.preventDefault();
+                $refs.search.focus();
+            }
+        "
+        @focus="open = true"
+        @keydown.escape.window="open = false"
+        @keydown="open = true"
+        @keydown.shift.tab="open = false">
     <div class="absolute top-0 flex items-center h-full ml-2">
         <svg class="fill-current text-gray-400 w-4" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
     </div>
@@ -15,12 +27,19 @@
 
 
     @if(strlen($search) >= 2)
-        <div class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2">
+        <div
+                class="absolute z-50 bg-gray-800 text-xs rounded w-64 mt-2"
+                x-show.transition.opacity.duration.200="open"
+        >
             @if(count($searchResult)>0)
                 <ul>
                     @foreach($searchResult as $game)
                         <li class="border-b border-gray-700">
-                            <a href="{{route('games.show', $game['slug'])}}" class="block hover:bg-gray-700 flex items-center transition ease-in-out duration-150 px-3 py-3">
+                            <a
+                                    href="{{route('games.show', $game['slug'])}}"
+                                    class="block focus:bg-gray-700 hover:bg-gray-700 flex items-center transition ease-in-out duration-150 px-3 py-3"
+                                    @if($loop->last) @keydown.tab="open = false" @endif
+                            >
                                 @if(array_key_exists('cover', $game))
                                     <img src="{{str_replace("thumb", "cover_small", $game['cover']['url'])}}" alt="Cover" class="w-10">
                                 @else
